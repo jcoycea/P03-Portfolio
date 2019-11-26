e55d6e08e901f6fdd92be7947ad022f1868df2c7
Joyce <jaquino@sfu.ca>
2019-11-26 10:23:01 -0800

    Add lazy loading to images

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,57 @@
-// var carouselContainers = document.querySelectorAll('.carousel-container');
-//
-// for ( var i=0; i < carouselContainers.length; i++ ) {
-//   var container = carouselContainers[i];
-//   initCarouselContainer( container );
-// }
-//
-// function initCarouselContainer( container ) {
-//   var carousel = container.querySelector('.carousel');
-//   var flkty = new Flickity( carousel );
-//   var carouselStatus = container.querySelector('.carousel-status');
-//
-//   function updateStatus() {
-//     var slideNumber = flkty.selectedIndex + 1;
-//     carouselStatus.textContent = slideNumber + '/' + flkty.slides.length;
-//   }
-//   updateStatus();
-//
-//   flkty.on( 'select', updateStatus );
-//
-// }
+/****
+
+Code From:
+- https://css-tricks.com/the-complete-guide-to-lazy-loading-images/
+- https://codepen.io/imagekit_io/pen/BPXQZZ
+
+****/
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages;
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    var imageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.src = image.dataset.src;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function(image) {
+      imageObserver.observe(image);
+    });
+  } else {
+    var lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }
+
+      lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) {
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  }
+})
